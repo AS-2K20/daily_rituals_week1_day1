35b4589915146d995e220ed4a0a82f01b51fd1ab
AS_ProGrad <66553883+AS-2K20@users.noreply.github.com>
2020-09-03 22:35:12 +0530

    Adding Files

---
+++ ALICE IN WONDERLAND/src/index.js
@@ -0,0 +1,22 @@
+// Alice was bored that day,so she was sitting on the riverbank.
+// Suddenly  she notices a talking, White Rabbit with a pocket watch. 
+// It ran fast,and she followed it, down a rabbit hole.
+// She fell into the hole and found a magical wonderland with dark trees, beautiful flowers.
+// She found many ways numbered from 1,2,3,........18.
+// she was confused which is the right way that will lead her to her home. 
+// She found a cute bird, standing in one of the tree. 
+// Alice asked the bird the way to go back to her home.
+// The bird said a two digit number( say 23 ) and asked her to find the sum of its digits (2+3=5) and that numbered way will lead her to her home.
+// Alice was already confused, so pls help Alice in finding the route to her home....
+
+
+//Write a function to return the route to her home
+function find_Route(number){
+    var n = number.toString();
+    var digit1= parseInt(n.charAt(0));
+    var digit2= parseInt(n.charAt(1));
+    var result = digit1+digit2;
+    return result;
+}
+
+//Run SpecRunner.html to check the Test Case. n = number.toString();
